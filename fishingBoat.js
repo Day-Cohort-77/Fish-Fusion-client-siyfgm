bc76f0c77638f5c1c6f329dc1a1296052f3173a0
Gavin Beaudet <gman.beaudet@gmail.com>
2025-05-14 11:25:16 -0400

    Code Unfinished

---
+++ fishingBoat.js
@@ -0,0 +1,11 @@
+import { fishes } from './database.js'
+
+const boatInventory = {
+    const fish = []
+
+    for (let counter = 0; counter < 3; counter++ ) {
+    const randomFishId = Math.floor(Math.random() * fishes.length
+    fish.push(fish[randomFishId])
+}
+    return fish
+}
